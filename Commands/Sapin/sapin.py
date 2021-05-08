1998239a83029f141c3d600067e5e88097f926d3
ismailAbdelwahab <is.abdelwahab@gmail.com>
2021-05-08 18:54:56 +0200

    Adding a command to throw christmas trees in the chat. 	You can for now just throw one (because why not). 	Or you can specify on what are you throwing that tree.

---
+++ Commands/Sapin/sapin.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+from bot_loader import STARTING_CHAR, bot
+
+@bot.command(
+	name="sapin",
+	help=f"Just enter \"{STARTING_CHAR}sapin\" or\n\
+	\"{STARTING_CHAR}sapin\" <Something> to throw a christmas tree at it.",
+	brief="Let the bot throw a christmas tree in the chat."
+)
+async def sapin(ctx, *args):
+	answer = f"> **{ctx.message.author.name}** is throwing a christams tree"
+	if args == (): # No args given
+		answer += "."
+	else:
+		answer += " at**"
+		for arg in args:
+			answer += f" {arg}"
+		answer += "**."
+	await ctx.channel.send( answer )
+	await ctx.channel.send("https://tenor.com/view/fk-this-i-quit-nope-i-cant-christmas-tree-gif-14498008")
