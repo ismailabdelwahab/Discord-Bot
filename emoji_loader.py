a7f14a3d5b9c3b1448cc5b883aa564cbc70bc235
ismailAbdelwahab <is.abdelwahab@gmail.com>
2021-05-10 16:33:41 +0200

    Adding some flags emojies.

--- emoji_loader.py
+++ emoji_loader.py
@@ -1,8 +1,17 @@
 #!/usr/bin/env python3
+
+# Success / Error :
 EMOJI_FORBIDEN = ":no_entry:"
 EMOJI_ERROR = ":x:"
 EMOJI_SUCCESS = ":white_check_mark:"
 
 
-# Characters:
+# Characters :
 EMOJI_SKULL = ":skull:"
+
+# Flags :	
+FALG_FR = ":flag_fr:"
+FALG_UK = ":flag_gb:"
+FALG_EG = ":flag_eg:"
+FALG_RU = ":flag_ru:"
+FALG_KR = ":flag_kr:"
